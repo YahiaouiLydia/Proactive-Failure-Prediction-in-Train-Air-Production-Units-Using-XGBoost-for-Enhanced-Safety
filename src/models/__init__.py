ea2af38b000c37f9966a795a24e655f9749379bc
YahiaouiLydia <lydia.yahiaoui@univ-bejaia.dz>
2025-04-16 16:04:40 +0100

    gitignore

--- src/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# src/models/__init__.py
-
-from .train import build_xgboost_model, build_rf_model, build_dt_model, build_svm_model,tune_xgboost_model,build_knn_model,build_nb_model, build_logistic_model
-from .evaluate import evaluate_model
--- src/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-# src/utils/__init__.py
-
-from .helpers import plot_roc_curve, plot_classification_report
